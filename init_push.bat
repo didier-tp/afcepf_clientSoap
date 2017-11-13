0e3e9f9ca4a63cbd3a9e4a52f7be22731bc2131a
didier <didier-defrance@wanadoo.fr>
2017-11-13 16:55:48 +0100

    mieux

---
+++ init_push.bat
@@ -0,0 +1,7 @@
+cd /d "%~dp0"
+git remote add gitHubOriginAfcepfClientSoap https://didier-tp:pwd007!@github.com/didier-tp/afcepf_clientSoap.git
+git push -u gitHubOriginAfcepfClientSoap master
+pause
+
+REM open with text editor
+REM opne with system editor
--- src/main/java/fr/afcepf/dja/client/ClientSoapApp.java
+++ src/main/java/fr/afcepf/dja/client/ClientSoapApp.java
@@ -1,5 +1,8 @@
 package fr.afcepf.dja.client;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import fr.afcepf.dja.ws.ServiceTva;
 import fr.afcepf.dja.ws.ServiceTvaImplService;
 
@@ -8,12 +11,22 @@ public class ClientSoapApp {
 	public static void main(String[] args) {
 		//code d'appel du WS SOAP qui s'appuie sur 
 		//le code généré par wsimport
-		ServiceTva wsTva = (new ServiceTvaImplService())
-				                      .getServiceTvaImplPort();
-        // (new ....Service).get...Port()
-		double resTva = wsTva.tva(200.0,20.0);
-		System.out.println("tva="+resTva);
-		String auteur = wsTva.getAuteur();
-		System.out.println("auteur="+auteur);
+		try {
+			//adresse ip 192.168.102.116 à déterminer via ipconfig ou ...
+			//String urlAsString="http://" + "192.168.102.116"
+			//                   + ":8080/serverSoap/ws/tva?wsdl";
+			String urlAsString="http://" + "localhost"
+	                   + ":8080/serverSoap/ws/tva?wsdl";
+			URL wsdlURL = new URL(urlAsString);
+			ServiceTva wsTva = (new ServiceTvaImplService(wsdlURL))
+					                      .getServiceTvaImplPort();
+			// (new ....Service).get...Port()
+			double resTva = wsTva.tva(200.0,20.0);
+			System.out.println("tva="+resTva);
+			String auteur = wsTva.getAuteur();
+			System.out.println("auteur="+auteur);
+		} catch (MalformedURLException e) {
+				e.printStackTrace();
+		}
 	}
 }
